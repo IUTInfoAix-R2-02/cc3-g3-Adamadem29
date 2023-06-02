75d02fad89e3e5722226e0945f0b90e2c2b86527
d22004772 <adam.DEANGELI@etu.univ-amu.fr>
2023-06-02 17:16:13 +0200

    Patie2 et 3 finie

--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -13,6 +13,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
@@ -38,6 +39,26 @@ public class ToileController implements Initializable {
 
     private TextField sourceOfEvent;
 
+    private ListView<Line> lesLignes;
+
+    @FXML
+    private Line cpt1Line;
+
+    @FXML
+    private Line cpt2Line;
+
+    @FXML
+    private Line cpt3Line;
+
+    @FXML
+    private Line cpt4Line;
+
+    @FXML
+    private Line cpt5Line;
+
+    @FXML
+    private Line cpt6Line;
+
     private ListView<Circle> lesCercles;
 
     @FXML
@@ -58,17 +79,40 @@ public class ToileController implements Initializable {
     @FXML
     private Circle cpt6Circle;
 
+    private ListView<TextField> lesTextField;
     @FXML
     private TextField cpt1Field;
 
     @FXML
-    private
+    private TextField cpt2Field;
+
+    @FXML
+    private TextField cpt3Field;
+
+    @FXML
+    private TextField cpt4Field;
+
+    @FXML
+    private TextField cpt5Field;
+
+    @FXML
+    private TextField cpt6Field;
+
+    @FXML
+    private Label errorLbl1;
+
+    @FXML
+    private Label errorLbl2;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         lesCercles = new ListView();
         lesCercles.getItems().addAll(cpt1Circle,cpt2Circle,cpt3Circle,cpt4Circle,cpt5Circle,cpt6Circle);
+        lesTextField = new ListView();
+        lesTextField.getItems().addAll(cpt1Field,cpt2Field,cpt3Field,cpt4Field,cpt5Field,cpt6Field);
+        lesLignes = new ListView();
+        lesLignes.getItems().addAll(cpt1Line,cpt2Line,cpt3Line,cpt4Line,cpt5Line,cpt6Line);
     }
 
     int getXRadarChart(double value, int axe ){
@@ -87,14 +131,55 @@ public class ToileController implements Initializable {
         int axe = GridPane.getRowIndex(sourceOfEvent)+1;
         double value = Integer.parseInt(sourceOfEvent.getText());
         if (value>20 || value<0){
-
+            errorLbl1.setVisible(true);
+            errorLbl2.setVisible(true);
+            return;
         }
+        errorLbl1.setVisible(false);
+        errorLbl2.setVisible(false);
         int centerX = getXRadarChart(value, axe);
         int centerY = getYRadarChart(value, axe);
         Circle leCercle = lesCercles.getItems().get(axe-1);
         leCercle.setCenterX(centerX);
         leCercle.setCenterY(centerY);
         leCercle.setVisible(true);
+    }
+
+    @FXML
+    public void handleViderButtonClick(ActionEvent event){
+        errorLbl1.setVisible(false);
+        errorLbl2.setVisible(false);
+        for (Circle cercle : lesCercles.getItems()){
+            cercle.setVisible(false);
+        }
+        for (TextField champ : lesTextField.getItems()){
+            champ.setText("");
+        }
+    }
+
+    @FXML
+    public void handleTracerButtonClick(ActionEvent event){
+        Line laLigne;
+        Circle lePoint1;
+        Circle lePoint2;
+        for (TextField champ : lesTextField.getItems()){
+            champ.setOnAction(handleOnActionField(event));
+        }
+        for (int i=0; i<lesLignes.getItems().size(); ++i){
+            laLigne = lesLignes.getItems().get(i);
+            lePoint1 = lesCercles.getItems().get(i);
+            if (i==5){
+                lePoint2 = lesCercles.getItems().get(0);
+            }
+            else{
+                lePoint2 = lesCercles.getItems().get(i+1);
+            }
+            laLigne.setStartX(lePoint1.getCenterX());
+            laLigne.setStartY(lePoint1.getCenterY());
+            laLigne.setEndX(lePoint2.getCenterX());
+            laLigne.setEndY(lePoint2.getCenterY());
+            laLigne.setVisible(true);
+        }
     }
 
 }
